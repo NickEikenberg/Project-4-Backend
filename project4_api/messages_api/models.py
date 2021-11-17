7f4c1210b8fd153dabd52e996d5663bb8d0a2d65
Nick Eikenberg <nickeikenberg95@gmail.com>
2021-11-17 12:00:26 -0500

    Updated Message model to have title and likes

--- project4_api/messages_api/models.py
+++ project4_api/messages_api/models.py
@@ -5,4 +5,6 @@ from django.db import models
 class Message(models.Model):
     name = models.CharField(max_length=32)
     message = models.CharField(max_length=280)
+    title = models.CharField(max_length=24, default='')
+    likes = models.IntegerField(default=0)
 
